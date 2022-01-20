88c57b31d5969e0322af4276e56cecbd49162eb0
Daan <giltchutoy81@gmail.com>
2022-01-20 07:00:40 -0600

    Modulo con la clase del marcador finalizado

--- score.py
+++ score.py
@@ -5,6 +5,29 @@ class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.write('SCORE: {}'.format(self.score), align = 'center')
-        
+        self.color("white")
+        self.pu()
+        self.goto(0,260)
+        self.hideturtle()
+        self.score_text
+    
+    @property
+    def score_text(self):
+        self.write('SCORE: {}'.format(self.score), align = 'center', font = ('Arial', 25, 'normal'))
+    
+    @property
+    def score_points(self):
+        self.score += 1
+        self.clear()
+        self.score_text
+    
+    @property
+    def game_over(self):
+        self.pu()
+        self.goto(0,0)
+        self.color('blue')
+        self.write('GAME OVER', align = 'center', font = ('Arial', 25, 'normal'))
+
+
+
 
